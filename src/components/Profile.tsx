bd76010c892fb3c50039535326ff8a0697b18463
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-11-11 13:51:07 -0400

    update profile section

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -33,57 +33,38 @@ export const Profile = () => {
         <Card>
             <CardContent className="pt-6">
                 <div className="flex flex-col items-start gap-2 ">
-                    <Image
-                        width={150}
-                        height={150}
-                        quality={100}
-                        alt="Avatar Icon"
-                        src="/avatar.svg"
-                        className="rounded-full border-2 mx-auto object-cover"
-                    />
-                    <h1 className="text-2xl mt-4 font-bold">Your Avatar</h1>
-                    <p className="text-muted-foreground font-medium">
-                        Solopreneur Developer
-                    </p>
+                    <div className="flex flex-row md:flex-col items-center md:items-start w-full gap-4">
+                        <Image 
+                            width={150}
+                            height={150}
+                            quality={100}
+                            src="/avatar.svg"
+                            alt="Profile Picture"
+                            className="rounded-full size-12 md:w-full h-auto object-cover border-2"
+                        />
+                        <div className="flex flex-col items-start justify-center">
+                            <h1 className="font-bold md:mt-4 text-xl md:text-2xl">Chris Lonzo</h1>
+                            <p className="text-sm md:text-base text-muted-foreground">
+                                Solopreneur Developer
+                            </p>
+                        </div>
+                    </div>
                     <p className="mt-2 text-start text-sm text-muted-foreground">
-                        Web Engineer and Digital Strategist on a mission to help businesses
-                        break free from ordinary websites and create digital experiences that drive growth.
+                        I am a solopreneur developer with a passion for building products that solve real-world problems.
                     </p>
-                    <Button
-                        asChild
-                        className="mt-4 w-full"
-                    >
-                        {/* Enter your email to get in touch */}
-                        {/* Or use Calendly or Cal to schedule a call */}
-                        <Link
-                            target="_blank"
+                    <Button className="mt-4 w-full" asChild>
+                        {/* TODO: Add resume link */}
+                        {/* or link to schedule a meeting using Calendly or Cal*/}
+                        <Link 
+                            target="_blank" 
+                            href="/resume.pdf" 
                             className="font-semibold"
-                            href="https://cal.com/chris-alonzo-u7ilrx/free-consultation-with-chris"
                         >
                             CONTACT ME
                         </Link>
                     </Button>
-                    {/* Social Links */}
-                    <div className="mt-4 flex flex-col space-y-2 border-t border-border pt-4 w-full">
-                        {socials.map((s, i) => {
-                            const parts = s.link.split("/");
-                            const username = parts[parts.length - 1];
-                            return (
-                                <Link
-                                    key={i}
-                                    href={s.link}
-                                    target="_blank"
-                                    aria-label={s.name}
-                                    className="cursor-pointer flex items-center gap-2 group"
-                                >
-                                    {s.icon}
-                                    <p className="text-sm text-muted-foreground group-hover:text-primary transition-color duration-200 ease-linear">
-                                        /{username}
-                                    </p>
-                                </Link>
-                            )
-                        })}
-                    </div>
+                    
+                    
                 </div>
             </CardContent>
         </Card>
