f1e2c8d43d66ca79b5c1f324d236fe2e38a222f3
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-11-11 14:02:31 -0400

    add social section to profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -34,7 +34,7 @@ export const Profile = () => {
             <CardContent className="pt-6">
                 <div className="flex flex-col items-start gap-2 ">
                     <div className="flex flex-row md:flex-col items-center md:items-start w-full gap-4">
-                        <Image 
+                        <Image
                             width={150}
                             height={150}
                             quality={100}
@@ -55,16 +55,34 @@ export const Profile = () => {
                     <Button className="mt-4 w-full" asChild>
                         {/* TODO: Add resume link */}
                         {/* or link to schedule a meeting using Calendly or Cal*/}
-                        <Link 
-                            target="_blank" 
-                            href="/resume.pdf" 
+                        <Link
+                            target="_blank"
+                            href="/resume.pdf"
                             className="font-semibold"
                         >
                             CONTACT ME
                         </Link>
                     </Button>
-                    
-                    
+                    <div className="mt-4 flex flex-col space-y-2 border-t border-border pt-4 w-full">
+                        {socials.map((s, i) => {
+                            const parts = s.link.split('/')
+                            const username = parts[parts.length - 1]
+                            return (
+                                <Link
+                                    key={i}
+                                    href={s.link}
+                                    target="_blank"
+                                    aria-label={`${s.name} Logo`}
+                                    className="cursor-pointer flex items-center gap-2 group"
+                                >
+                                    {s.icon}
+                                    <p className="text-sm text-muted-foreground group-hover:text-primary transition-color duration-200 ease-linear">
+                                        /{username}
+                                    </p>
+                                </Link>
+                            )
+                        })}
+                    </div>
                 </div>
             </CardContent>
         </Card>
